a43fbee1580b24e352a6603c78b92ecd72ae144f
Amit K Gupta <amitamit281@rediffmail.com>
2017-08-18 22:31:36 -0700

    Enable CORS

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -4,8 +4,10 @@ import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
