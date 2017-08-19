f28742421e29bf2f63c88645bcf079f72b99f641
Amit K Gupta <amitamit281@rediffmail.com>
2017-08-18 23:27:54 -0700

    xc

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -4,11 +4,14 @@ import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@CrossOrigin(origins="*",methods= {RequestMethod.OPTIONS,RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT},allowedHeaders= "*")
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
@@ -16,5 +19,15 @@ public class Application extends SpringBootServletInitializer {
                 initializers(new SpringApplicationContextInitializer())
                 .application()
                 .run(args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowCredentials(false).maxAge(3600);
+            }
+        };
     }
 }
